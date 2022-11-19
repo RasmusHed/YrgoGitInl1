b62c4f7f0efa1ca3936a0cbf56b76be0b3dafa9a
Rasmus Hedin <Rashed1110@skola.goteborg.se>
2022-11-19 12:39:36 +0100

    Changing max value to 10

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser runGuesser = new Guesser(1, 50);
+        Guesser runGuesser = new Guesser(1, 10);
         runGuesser.start();
     }
 }
